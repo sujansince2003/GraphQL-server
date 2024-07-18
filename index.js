0e65d9249218d1e62af128a8c3175632d4462e60
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2024-07-18 09:17:53 +0545

    intial server setup

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// in this file we will setup apollo server for graphql
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+// setting up server
+
+const server = new ApolloServer({});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
