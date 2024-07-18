ead4ebf04687d313809b5aa470be35fec9f1d403
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2024-07-18 16:54:46 +0545

    passing typeDefs as parameter to ApolloServer

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
+//types
+import { typeDefs } from "./schema";
+
 // setting up server
 
-const server = new ApolloServer({});
+const server = new ApolloServer({
+  typeDefs, //this will tell ApolloServer about  data types and entry points available
+});
 
 const { url } = await startStandaloneServer(server, {
   listen: { port: 4000 },
