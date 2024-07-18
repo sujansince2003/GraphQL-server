9918ffa3badb6d551dc73acf3b4db9f1ca5e6510
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2024-07-18 09:18:26 +0545

    defining schema for dataobject and type Query for entry point

---
+++ schema.js
@@ -0,0 +1,32 @@
+//we are exporting typeDefs to pass it as argument in ApolloServer in index.js
+
+export const typeDefs = `#graphql 
+# we are having 3 data object game,review and auth object
+
+type Game{
+id:ID!,       #   if we add ! it means it cannot be null   
+  title:String,
+  platform: [String!]!,
+  
+}
+
+type Review{
+    id:ID! 
+    rating: Int! 
+    content: String!
+
+}
+
+type Author {
+    id:ID! 
+    fname: String!
+    verified:Boolean!
+}
+#we have made our data type schema but need to create one more special type i.e Query.its not optional. it is used to define the entry point to the graph and specify the return types of those entries.  user can only access to those data object specified inside Query type as return item. for eg 
+
+type Query{
+    reviews:[Review]
+    games: [Game]
+    authors:[Author]
+}
+`;
