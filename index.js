1fad8705b2aef87a2fb5da0a1e833f3ad76f39a7
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2024-07-18 17:32:00 +0545

    added resolvers to handle queries

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import db from "./_db.js";
 
 //types
-import { typeDefs } from "./schema";
+import { typeDefs } from "./schema.js";
+
+//defing the resolver functions
+const resolvers = {
+  Query: {
+    //the keyword Query should exact match to type we defined in schema or typeDefs
+    games() {
+      //this is resolver function for games and the keyword games should exact match to type we defined in schema or typeDefs inside type Query
+      return db.games; //in typeQuery it is defined to return array so it also need to return array here too. and we may only need title of game from array of objects of gamedata but apollo will handle itself.all it need to know is where to grab data from
+    },
+    reviews() {
+      return db.reviews;
+    },
+    authors() {
+      return db.authors;
+    },
+  },
+};
 
 // setting up server
 
 const server = new ApolloServer({
   typeDefs, //this will tell ApolloServer about  data types and entry points available
+  resolvers,
 });
 
 const { url } = await startStandaloneServer(server, {
   listen: { port: 4000 },
 });
+
+console.log(`Server ready at: ${url}`);
