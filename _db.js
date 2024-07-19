97b3eeeb02554b0298d8d087d37c1bf1c2772440
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2024-07-19 10:01:03 +0545

    added resolvers functions to get related data

--- _db.js
+++ _db.js
@@ -13,13 +13,31 @@ let authors = [
 ];
 
 let reviews = [
-  { id: "1", rating: 9, content: "lorem ipsum", author_id: "1", game_id: "2" },
-  { id: "2", rating: 10, content: "lorem ipsum", author_id: "2", game_id: "1" },
+  {
+    id: "1",
+    rating: 9,
+    content: "game of id 2 is just too awesome",
+    author_id: "1",
+    game_id: "2",
+  },
+  {
+    id: "2",
+    rating: 10,
+    content: "game of id 1 and author id 2 is just too awesome",
+    author_id: "2",
+    game_id: "1",
+  },
   { id: "3", rating: 7, content: "lorem ipsum", author_id: "3", game_id: "3" },
   { id: "4", rating: 5, content: "lorem ipsum", author_id: "2", game_id: "4" },
   { id: "5", rating: 8, content: "lorem ipsum", author_id: "2", game_id: "5" },
   { id: "6", rating: 7, content: "lorem ipsum", author_id: "1", game_id: "2" },
-  { id: "7", rating: 10, content: "lorem ipsum", author_id: "3", game_id: "1" },
+  {
+    id: "7",
+    rating: 10,
+    content: "review author id 3 game 2",
+    author_id: "3",
+    game_id: "2",
+  },
 ];
 
 export default { games, authors, reviews };
--- index.js
+++ index.js
@@ -32,6 +32,25 @@ const resolvers = {
       return db.authors.find((author) => author.id === args.id);
     },
   },
+
+  Game: {
+    reviews(parent) {
+      return db.reviews.filter((review) => review.game_id === parent.id);
+    },
+  },
+  Author: {
+    reviews(parent) {
+      return db.reviews.filter((review) => review.author_id === parent.id);
+    },
+  },
+  Review: {
+    game(parent) {
+      return db.games.find((game) => game.id === parent.game_id);
+    },
+    author(parent) {
+      return db.authors.find((author) => author.id === parent.author_id);
+    },
+  },
 };
 
 // setting up server
